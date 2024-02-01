4e505d905a47a6bccefc54b3d047f63b09e57851
0xb10b <0xb10b@proton.me>
2024-02-01 16:03:47 -0500

    added hostchanger

---
+++ change_hosts.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def modify_hosts_file(new_hostname):
+    hosts_file = open('/etc/hosts', 'r')
+    old_hosts = hosts_file.readlines()
+    hosts_file.close()
+
+    new_hosts = []
+    for line in old_hosts:
+        if '127.0.1.1' in line:
+            print('[+] Modifying /etc/hosts: ')
+            print(f'    old: {line[:-1]}')
+            new_hosts.append(line.replace(line.split('\t')[-1], new_hostname + '\n'))
+            print(f'    new: {new_hosts[-1][:-1]}')
+        else:
+            new_hosts.append(line)
+
+    hosts_file = open('/etc/hosts', 'w')
+    for line in new_hosts:
+        hosts_file.write(line)
+    hosts_file.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print(f'Usage: {sys.argv[0]} <new-hostname>')
+        exit()
+
+    modify_hosts_file(sys.argv[1])
--- place_bins.sh
+++ place_bins.sh
@@ -7,8 +7,12 @@ txtblu=$(tput setaf 4) # blue
 echo -e "${txtgrn}[!]${txtoc} Begin placing scripts...\n";
 
 
-echo -e "${txtblu}[+]${txtoc} Placing anonymise, deanonymise in /usr/bin";
+echo -e "${txtblu}[+]${txtoc} Copying anonymise, deanonymise in /usr/bin";
 cp anonymise deanonymise /usr/bin/;
 chmod +x /usr/bin/anonymise /usr/bin/deanonymise;
+
+echo -e "${txtblu}[+]${txtoc} Copying change_hosts.py in /usr/local/bin";
+cp change_hosts.py /usr/local/bin;
+chmod +x /usr/local/bin/change_hosts.py
 
 echo -e "\n${txtgrn}[!]${txtoc} Scripts placed!";
